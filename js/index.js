18cc895a3228e4fe6ba4463f9b28d67148c55cc8
Ho Yun <115262503+HYC0819@users.noreply.github.com>
2022-10-09 15:34:10 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,6 +3,18 @@ AOS.init()
 var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
 var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
 function number2words(n){
     if (n < 20) return num[n];
     var digit = n%10;
@@ -15,57 +27,67 @@ function calculateAge(birthday) {
     var ageDifMs = Date.now() - birthday;
     var ageDate = new Date(ageDifMs);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+}
 
-anime({
-    targets: '.title:first-of-type',
-    translateY: -window.innerHeight/7,
-    delay: 1500
-}); 
+// Scene One
 
-var typewriter = new Typewriter(document.getElementsByClassName("title")[1], {
-    loop: false
-});
-
-setTimeout(() => {
-    typewriter.typeString("My name is Ho Yun")
-    typewriter.start()
-
-}, 3000)
-
-setTimeout(() => {
-    journeyButton = document.createElement("div")
-    journeyButton.textContent = "My Journey"
-    journeyButton.classList.add("journey")
-    journeyButton.setAttribute("data-aos", "zoom-in")
-    journeyButton.setAttribute("data-aos-duration", "1500")
-    document.body.appendChild(journeyButton)
-
-    var animation = anime({
-        targets: '.journey',
-        translateX: 1*window.innerWidth,
-        translateY: -1*window.innerHeight,
-        delay: function (el, i) { return i * 100; },
-        duration: 500,
-        direction: 'alternate',
-        loop: false,
-        autoplay: false,
-        easing: 'easeInOutQuad',
-        complete: function(anim) {
-            anime({
-                delay: 500,
-                targets: ".wiper",
-                translateX: -window.innerWidth,
-                duration: 2000,
-                easing: 'easeInOutQuad',
-                complete: function(anim2) {
-                    document.body.style.backgroundColor = "orange"
-                    document.body.innerHTML = ""
-                }
-            })
-        }
-    });
+function SceneOne() {
+    anime({
+        targets: '.title:first-of-type',
+        translateY: -window.innerHeight/7,
+        delay: 1500
+    }); 
     
+    setTimeout(() => {
+        var typewriter = new Typewriter(document.getElementsByClassName("title")[1], {
+            loop: false
+        });
+        typewriter.typeString("My name is Ho Yun")
+        typewriter.start()
     
-    document.querySelectorAll(".journey").forEach((el)=> el.onclick = animation.play);
-}, 6000)
+    }, 3000)
+    
+    setTimeout(() => {
+        journeyButton = document.createElement("div")
+        journeyButton.textContent = "About Me"
+        journeyButton.classList.add("journey")
+        journeyButton.setAttribute("data-aos", "zoom-in")
+        journeyButton.setAttribute("data-aos-duration", "1500")
+        document.body.appendChild(journeyButton)
+    
+        var animation = anime({
+            targets: '.journey',
+            translateX: window.innerWidth,
+            translateY: -window.innerHeight,
+            delay: function (el, i) { return i * 100; },
+            duration: 500,
+            direction: 'alternate',
+            loop: false,
+            autoplay: false,
+            easing: 'easeInOutQuad',
+            complete: function(anim) {
+                anime({
+                    delay: 500,
+                    targets: ".wiper",
+                    translateX: -window.innerWidth,
+                    duration: 1200,
+                    easing: 'easeInOutQuad',
+                    complete: function(anim2) {
+                        document.body.style.backgroundColor = "orange"
+                        readTextFile("secondScene.html", function(text){
+                            document.body.innerHTML = text
+                        });
+                        var script = document.createElement('script');
+                        script.src = "js/secondScene.js";
+                        document.body.appendChild(script)
+                    }
+                })
+            }
+        });
+        
+        
+        document.querySelectorAll(".journey").forEach((el)=> el.onclick = animation.play);
+    }, 6000)
+}
+
+SceneOne()
---
+++ js/secondScene.js
@@ -0,0 +1,40 @@
+AOS.init()
+setTimeout(() => {
+    document.getElementById("thirdLine").classList.add("show")
+    document.getElementById("backend").classList.add("show")
+}, 3000)
+anime({
+    delay: 2500,
+    targets: "#frontEndGif",
+    translateY: -screen.height,
+    easing: 'easeInOutQuad',
+})
+
+anime({
+    delay: 3500,
+    targets: "#backEndGif",
+    translateY: -screen.height,
+    easing: 'easeInOutQuad',
+    duration: 1000,
+    complete: function(anim2) {
+        anime({
+            delay: 3000,
+            targets: ".wiper",
+            translateY: window.innerHeight,
+            duration: 1000,
+            easing: 'easeInOutQuad',
+            complete: function(anim3) {
+                document.body.style.backgroundColor = "rgb(0, 95, 133)"
+                readTextFile("thirdScene.html", function(text){
+                    document.body.innerHTML = text
+                });
+                var script = document.createElement('script');
+                script.src = "js/thirdScene.js";
+                document.body.appendChild(script)
+                var script2 = document.createElement('script');
+                script2.src = "js/repo-card.js";
+                document.body.appendChild(script2)
+            }
+        })
+    }
+})
